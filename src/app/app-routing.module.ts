84ccda49d20126c6f68c132134dbb2eada138b00
daoson12 <daoduvictor48@gmail.com>
2020-08-30 15:28:45 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './layout/landing-page/landing-page.component';
+import { LoginComponent } from './security/login/login.component';
+import { SignUpComponent } from './security/sign-up/sign-up.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [ 
+  { path: '', component: LandingPageComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'sign-up', component: SignUpComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './layout/landing-page/landing-page.component';
+import { HomeComponent } from './layout/home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { LoginComponent } from './security/login/login.component';
+import { SignUpComponent } from './security/sign-up/sign-up.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    SidebarComponent,
+    LoginComponent,
+    SignUpComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
